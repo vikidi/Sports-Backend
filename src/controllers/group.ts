b82dee0275b148e6246d48006140e7db9d1071a4
Ville Saarinen <ville.saarinen9@gmail.com>
2024-10-30 09:11:35 +0200

    TS migration, group controller

--- src/controllers/group.ts
+++ src/controllers/group.ts
@@ -1,86 +1,113 @@
 export {}; // This is to combat the TS2451 error
 
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import Group from "../models/group";
 import Route from "../models/route";
+import { AppError, HttpCode } from "../exceptions/AppError";
 
-const create = (req: Request, res: Response) => {
-  let createdGroup: any; // TODO: fix
-  Group.create({
+export const create = async (
+  req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  const group = await Group.create({
     user: req.user!.id,
     route: req.body.routeId,
     exercises: [],
-  })
-    .then((data) => {
-      const { user, ...newData } = data;
-      createdGroup = newData;
-      return Route.findById(req.body.routeId);
-    })
-    .then((route) => {
-      if (!route!.groups.length) route!.defaultGroup = createdGroup._id;
-      route!.groups.push(createdGroup._id);
-      return route!.save();
-    })
-    .then(() => {
-      return res.json(createdGroup);
-    })
-    .catch((err) => {
-      res.status(err.status ?? 500).json({ errors: [err.message] });
-    });
+  });
+
+  const { user, ...createdGroup } = group;
+  const route = await Route.findById(req.body.routeId);
+
+  if (!route!.groups.length) route!.defaultGroup = createdGroup._id;
+  route!.groups.push(createdGroup._id);
+
+  await route!.save();
+
+  res.json(createdGroup);
 };
 
-const myList = (req: Request, res: Response) => {
-  Group.find({ user: req.user!.id }, "exercises name description")
-    .exec()
-    .then((data) => {
-      return res.json(data);
-    })
-    .catch((err) => {
-      res.status(err.status ?? 500).json({ errors: [err.message] });
-    });
+export const myList = async (
+  req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  const group = await Group.find(
+    { user: req.user!.id },
+    "exercises name description"
+  ).exec();
+
+  res.json(group);
 };
 
-const getOne = (req: Request, res: Response) => {
-  Group.findById(req.params.id, "user route exercises name description")
-    .populate(
-      "exercises",
-      "sport startingEpoch parsedDate averageHeartRate averagePace averageCadence averageWatts distanceMeters elapsedSec"
-    )
-    .then((data) => {
-      if (!data) return res.sendStatus(404);
-      if (data.user !== req.user!.id) return res.sendStatus(403);
+export const getOne = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const group = await Group.findById(
+    req.params.id,
+    "user route exercises name description"
+  ).populate(
+    "exercises",
+    "sport startingEpoch parsedDate averageHeartRate averagePace averageCadence averageWatts distanceMeters elapsedSec"
+  );
 
-      const { user, ...sendData } = data;
-      return res.json(sendData);
-    })
-    .catch((err) => {
-      res.status(err.status ?? 500).json({ errors: [err.message] });
-    });
-};
-
-const deleteOne = async (req: Request, res: Response) => {
-  try {
-    const group = await Group.findById(req.params.id);
-
-    if (group == null || group.user !== req.user!.id)
-      return res.status(403).json({ errors: ["Not authorized."] });
-
-    const route = await Route.findById(group); // TODO: Not working! Find the route for the group
-
-    route!.groups = route!.groups.filter(
-      (x) => x._id.toString() !== req.params.id
+  if (!group) {
+    return next(
+      new AppError({
+        httpCode: HttpCode.NOT_FOUND,
+        description: "Group not found.",
+      })
     );
-    await route!.save();
-    await group.deleteOne();
-    return res.json({});
-  } catch (error: any) {
-    return res.status(error.status ?? 500).json({ errors: [error.message] });
   }
+
+  if (group.user !== req.user!.id) {
+    return next(
+      new AppError({
+        httpCode: HttpCode.FORBIDDEN,
+        description: "User unauthorized.",
+      })
+    );
+  }
+
+  const { user, ...sendData } = group;
+  res.json(sendData);
 };
 
-module.exports = {
-  create,
-  myList,
-  getOne,
-  deleteOne,
+export const deleteOne = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const group = await Group.findById(req.params.id);
+
+  if (!group) {
+    return next(
+      new AppError({
+        httpCode: HttpCode.NOT_FOUND,
+        description: "Group not found.",
+      })
+    );
+  }
+
+  if (group.user !== req.user!.id) {
+    return next(
+      new AppError({
+        httpCode: HttpCode.FORBIDDEN,
+        description: "User unauthorized.",
+      })
+    );
+  }
+
+  const route = await Route.findById(group); // TODO: Not working! Find the route for the group
+
+  route!.groups = route!.groups.filter(
+    (x) => x._id.toString() !== req.params.id
+  );
+
+  await route!.save();
+  await group.deleteOne();
+
+  res.status(HttpCode.NO_CONTENT).json();
 };
